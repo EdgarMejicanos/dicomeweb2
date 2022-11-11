293459a8d0e4427b3b6ac154b4b6f901d1610921
edgarmejicanos <edgarsuchi2014@gmail.com>
2022-11-10 22:02:52 -0600

    Agregando estadisticas a vista home

--- app/Http/Controllers/pages/HomePage.php
+++ app/Http/Controllers/pages/HomePage.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers\pages;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\{Cliente, User};
+use Ramsey\Uuid\Type\Integer;
 
 class HomePage extends Controller
 {
   public function index()
   {
-    
+    //$n_cliente = Cliente::where('active', true)->count();
+    //$n_user = User::where('active', true)->count();
+
     return view('content.pages.pages-home');
   }
 }
--- resources/views/content/pages/pages-home.blade.php
+++ resources/views/content/pages/pages-home.blade.php
@@ -19,4 +19,63 @@ $configData = Helper::appClasses();
 @role('vendedor')
 <h2>Contenido solo para vendedor</h2>
 @endrole
+<div class="row">
+  <div class="col-xl-2">
+    <div class="card">
+      <div class="card-body text-center">
+        <div class="avatar avatar-md mx-auto mb-3">
+          <span class="avatar-initial rounded-circle bg-label-info"><i class="bx bx-edit fs-3"></i></span>
+        </div>
+        <span class="d-block mb-1 text-nowrap">Clientes</span>
+        <h2 class="mb-0">0</h2>
+      </div>
+    </div>
+  </div>
+  <div class="col-xl-2">
+    <div class="card">
+      <div class="card-body text-center">
+        <div class="avatar avatar-md mx-auto mb-3">
+          <span class="avatar-initial rounded-circle bg-label-warning"><i class="bx bx-dock-top fs-3"></i></span>
+        </div>
+        <span class="d-block mb-1 text-nowrap">Usuarios</span>
+        <h2 class="mb-0">0</h2>
+      </div>
+    </div>
+  </div>
+  <div class="col-xl-4">
+    <div class="card">
+      <div class="card-body text-center">
+        <div class="avatar avatar-md mx-auto mb-3">
+          <span class="avatar-initial rounded-circle bg-label-danger"><i class="bx bx-message-square-detail fs-3"></i></span>
+        </div>
+        <span class="d-block mb-1 text-nowrap">Boletas</span>
+        <h2 class="mb-0">0</h2>
+      </div>
+    </div>
+  </div>
+  <div class="col-xl-2 ">
+    <div class="card">
+      <div class="card-body text-center">
+        <div class="avatar avatar-md mx-auto mb-3">
+          <span class="avatar-initial rounded-circle bg-label-primary"><i class="bx bx-cube fs-3"></i></span>
+        </div>
+        <span class="d-block mb-1 text-nowrap">Sales</span>
+        <h2 class="mb-0">0</h2>
+      </div>
+    </div>
+  </div>
+  <div class="col-xl-2 ">
+    <div class="card">
+      <div class="card-body text-center">
+        <div class="avatar avatar-md mx-auto mb-3">
+          <span class="avatar-initial rounded-circle bg-label-success"><i class="bx bx-purchase-tag fs-3"></i></span>
+        </div>
+        <span class="d-block mb-1 text-nowrap">Purchase</span>
+        <h2 class="mb-0">0</h2>
+      </div>
+    </div>
+  </div>
+
+</div>
+
 @endsection
